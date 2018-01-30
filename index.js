b940950a92b8359543d934403b748bb1c7f5139d
Karan Nijhawan <karannijhawan@gizmee.com>
2018-01-30 12:14:33 +0100

    get isntan func

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ module.exports.refresh = function() {}
 module.exports.help = function() {
   help();
 }
-module.exports.getInstance () = function () {
+module.exports.getInstance = function () {
   return panini
 }
